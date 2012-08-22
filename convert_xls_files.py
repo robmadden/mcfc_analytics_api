2768ade146082139c1542db729221de70748d386
Rob Madden <madden.rob@gmail.com>
2012-08-22 00:36:46 -0500

    Database connection working.

--- convert_xls_files.py
+++ convert_xls_files.py
@@ -2,37 +2,49 @@ import os
 from django.core.management import setup_environ
 
 import settings
+import logging
 
 setup_environ(settings)
 
 import xlrd
 from django.db import connection, models
 
+logger = logging.getLogger(__name__)
 
-workbook = xlrd.open_workbook('../../mcfc_data/data.xls')
-sheets = workbook.sheets()
-for sheet in sheets:
+def create_table(sheet):
     name = sheet.name     # Table name
 
-    for row_num in range(sheet.nrows):
-        values = sheet.row_values(row_num) 
+    # The first row has the column names we want
+    row_values = sheet.row_values(0)
 
-        if row_num == 0:
-            column_map = {}
-            for column in sheet.row_values(row_num):
-               column_map[column] = "INT"
+    column_map = {}
+    for column in row_values:
+       column_map[column] = "INT"
 
-        create_columns_sql = ''
-        count = 0
-        for k,v in column_map.iteritems():
-            k = k.replace(' ', '_').lower()
-            create_columns_sql += k + " " + v
-            if count != sheet.ncols-1:
-                create_columns_sql += ", "
+    create_columns_sql = ''
+    count = 0
+    for k,v in column_map.iteritems():
+        k = k.replace(' ', '_').lower()
+        create_columns_sql += k + " " + v
+        if count != sheet.ncols-1:
+            create_columns_sql += ", "
 
-            count += 1
+        count += 1
 
-        sql = 'CREATE TABLE %s ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, %s) type=innodb;' % (name, create_columns_sql)
-        print sql
-        cursor = connection.cursor()
-        # cursor.execute(sql)
+    sql = 'CREATE TABLE %s ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, %s) type=innodb;' % (name, create_columns_sql)
+    logger.info(sql)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(sql)
+        print ("Successfully created table: %s" % name)
+    except Exception as e:
+        print ("Failed to create table: %s, %s" % (name, e))
+
+def main():
+    workbook = xlrd.open_workbook('../../mcfc_data/data.xls')
+    sheets = workbook.sheets()
+    for sheet in sheets:
+        create_table(sheet)
+
+if __name__ == "__main__":
+    main()
