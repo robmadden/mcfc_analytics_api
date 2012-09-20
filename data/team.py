cb0dfbb2b3fba67020d477888a0dc71f63d12aec
Rob Madden <madden.rob@gmail.com>
2012-09-19 23:47:21 -0500

    Added more complexity to team module.

--- data/team.py
+++ data/team.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from db_helpers import run_multirow_query, run_singlerow_query
 
 class Team():
@@ -21,37 +22,93 @@ class Team():
 
         (goals_scored, goals_conceded) = run_singlerow_query(sql, (self.team_id, opposition_id, date))
 
-        if goals_scored > goals_conceded:
+        if int(goals_scored) > int(goals_conceded):
             return "win"
-        elif goals_scored == goals_conceded:
+        elif int(goals_scored) == int(goals_conceded):
             return "tie"
         else:
             return "loss"
+
+    def get_record(self):
+        """
+        Get season results for a team
+
+        :returns: { 'wins': wins, 'losses': losses, 'ties': ties }
+        """
+        wins = losses = ties = 0
+        results = self.get_season_results()
+
+        for r in results:
+            result = r.get('result')
+            if result == 'win':
+                wins += 1
+            elif result == 'loss':
+                losses += 1
+            else:
+                ties += 1
+
+        return {
+            'wins': wins,
+            'losses': losses,
+            'ties': ties
+        }
 
     def get_season_results(self):
         """
         Get all fixtures for a given team
         
-        :returns: ( (date, team, team_id, opposition, opposition_id, venue, formation), ... )
+        :returns: ( (date, opposition, opposition_id, venue, formation), ... )
         """
 
         sql = """SELECT DISTINCT date, team_id, team, opposition_id, opposition, venue, team_formation
                  FROM players WHERE team_id=%s ORDER BY DATE;"""
 
-        results = run_multirow_query(sql, self.team_id)
-        results_with_results = []
-        for r in results:
-            r_with_result = {
-                'date': r[0],
-                'team_id': r[1],
-                'team': r[2],
-                'opposition_id': r[3],
-                'opposition': r[4],
-                'venue': r[5],
-                'formation': r[6],
-                'result': self.get_match_result(r[3], r[0])
+        matches = run_multirow_query(sql, self.team_id)
+        matches_with_results = []
+        for m in matches:
+            m_with_result = {
+                'date': m[0],
+                'opposition_id': m[3],
+                'opposition': m[4],
+                'venue': m[5],
+                'formation': m[6],
+                'result': self.get_match_result(m[3], m[0])
             }
             
-            results_with_results.append(r_with_result)
+            matches_with_results.append(m_with_result)
 
-        return results_with_results
+        return matches_with_results
+
+    def get_winning_pct(self):
+        """
+        Compute the winning percentage for a team
+
+        :returns: decimal win percentage
+        """
+
+        results = self.get_season_results()
+        games_played = len(results)
+        total_wins = 0
+
+        for r in results:
+            if r.get('result') == 'win':
+                total_wins += 1
+                
+        return total_wins / games_played
+
+    def get_winning_pct_with_formation(self, formation_id):
+        """
+        Compute the winning percentage for a team given a certain formation
+        
+        :returns: decimal win percentage
+        """
+
+        results = self.get_season_results()
+        games_played = len(results)
+        total_wins = 0
+        
+        for r in results:
+            if int(r.get('formation')) == int(formation_id) and r.get('result') == 'win':
+                total_wins += 1
+
+        return total_wins / games_played
