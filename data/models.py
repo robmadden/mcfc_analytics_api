272582d822e1539c23a4af0309446a89941555bb
Rob Madden <madden.rob@gmail.com>
2012-09-19 20:52:29 -0500

    Removed unused app, added more queries.

--- data/models.py
+++ data/models.py
@@ -1,6 +1,6 @@
 from django.db import models, connection
 
-class Players(models.Model):
+class Data(models.Model):
     _db = 'mcfc_analytics'
 
     id = models.AutoField(primary_key=True)
@@ -9,29 +9,57 @@ class Players(models.Model):
     team = models.CharField(max_length=50, null=True, blank=True)
     player_id = models.IntegerField(max_length=11)
 
+    def _run_query(self, query, params=None):
+        """
+        Run a generic query and return the results
+        """
+        cursor = connection.cursor()
+
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+            
+        results = cursor.fetchall()
+        cursor.close()
+        return results
+
     def get_teams(self):
         """
         Fetch all the teams and their corresponding IDs
 
-        @returns [ (team_id, team_name), (team_id, team_name), ... ]
+        :rtype: a list of tuples
+        :returns: [ (team_id, team_name), (team_id, team_name), ... ]
         """
 
-        sql = "select team_id, team from players group by team_id;"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        teams = cursor.fetchall()
-        cursor.close()
-        return teams
+        sql = "select distinct team_id, team from players order by team;"
+        return self._run_query(sql)
+
+    def get_players(self):
+        """
+        Fetch all the players in the league and their corresponding IDs
+
+        :rtype: a list of tuples
+        :returns: [ (player_id, player, team) ]
+        """
+        sql = "select distinct player_id, player_surname, player_forename, team_id, team from players order by team_id, player_surname;"
+        return self._run_query(sql)
 
     def get_players_on_team(self, team_id):
         """
         Get all players on a team given a team ID
 
-        @returns [ (player_id, player_surname, player_forname), ... ]
+        :returns: [ (player_id, player_surname, player_forname), ... ]
         """
         sql = 'select player_id, player_surname, player_forename, team from players where team_id=%s group by player_id, team_id;'
-        cursor = connection.cursor()
-        cursor.execute(sql, team_id)
-        players = cursor.fetchall()
-        cursor.close()
-        return players
+        return self._run_query(sql, team_id)
+
+    def get_team_fixtures(self, team_id):
+        """
+        Get all fixtures for a given team
+
+        :returns: ( (date, team, team_id, opposition, opposition_id, venue), ... )
+        """
+
+        sql = 'select date, team_id, team, opposition_id, opposition, venue from players where team_id=%s group by team_id, opposition_id;'
+        return self._run_query(sql, team_id)
